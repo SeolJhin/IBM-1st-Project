08fa64a277e239defe2860cc562a097c2ac25db5
SeolJhin <rjw0417@gmail.com>
2026-02-11 17:55:12 +0900

    wayhome

---
+++ uni_place_v1_0.0.0_1/src/main/java/org/myweb/uniplace/domain/affiliate/api/AffiliateController.java
@@ -0,0 +1,5 @@
+package org.myweb.uniplace.domain.affiliate.api;
+
+public class AffiliateController {
+
+}
---
+++ uni_place_v1_0.0.0_1/src/main/java/org/myweb/uniplace/domain/affiliate/api/dto/request/AffiliateCreateRequest.java
@@ -0,0 +1,5 @@
+package org.myweb.uniplace.domain.affiliate.api.dto.request;
+
+public class AffiliateCreateRequest {
+
+}
