364339e8a50a25720eecfce55cfdcec4b6a0c790
Seoly <rjw0417@gmail.com>
2026-02-18 21:41:58 +0900

    login

--- uni-place-frontend/src/App.js
+++ uni-place-frontend/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Home from "./shared/pages/Home";
+import Login from "./features/user/pages/Login";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
