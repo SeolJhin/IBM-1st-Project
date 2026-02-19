2f10d9a122d1d0babedfd2e7946d185fe3d6cb9f
hwangza125 <cutegirlji4282@gmail.com>
2026-02-19 12:49:56 +0900

    semi final user

--- uni-place-backend/src/main/java/org/myweb/uniplace/domain/user/application/UserService.java
+++ uni-place-backend/src/main/java/org/myweb/uniplace/domain/user/application/UserService.java
@@ -1,5 +1,7 @@
 package org.myweb.uniplace.domain.user.application;
 
+import org.myweb.uniplace.domain.user.api.admin.dto.request.AdminUserRoleUpdateRequest;
+import org.myweb.uniplace.domain.user.api.admin.dto.request.AdminUserStatusUpdateRequest;
 import org.myweb.uniplace.domain.user.api.dto.request.UserUpdateRequest;
 import org.myweb.uniplace.domain.user.api.dto.response.UserResponse;
 
@@ -7,5 +9,12 @@ public interface UserService {
     UserResponse me(String userId);
     UserResponse updateMe(String userId, UserUpdateRequest req);
     UserResponse getByIdForAdmin(String userId);
+    
+    // ✅ 관리자 - 상태 변경
+    UserResponse updateStatus(String userId, AdminUserStatusUpdateRequest req);
+
+    // ✅ 관리자 - 권한 변경
+    UserResponse updateRole(String userId, AdminUserRoleUpdateRequest req);
+    
     void deleteMe(String userId);
 }
