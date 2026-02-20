5246a966991f4a408bcc8a6a222baef0a606352a
jungbin845 <ijeongbin845@gmail.com>
2026-02-20 10:44:34 +0900

    product 2026/02/20-10:42 제작

--- uni-place-backend/src/main/java/org/myweb/uniplace/domain/commerce/application/ProductService.java
+++ uni-place-backend/src/main/java/org/myweb/uniplace/domain/commerce/application/ProductService.java
@@ -1,5 +1,22 @@
 package org.myweb.uniplace.domain.commerce.application;
 
-public class ProductService {
+import org.myweb.uniplace.domain.commerce.api.dto.request.ProductCreateRequest;
+import org.myweb.uniplace.domain.commerce.api.dto.request.ProductUpdateRequest;
+import org.myweb.uniplace.domain.commerce.api.dto.response.ProductResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+public interface ProductService {
+
+    Page<ProductResponse> list(String keyword, Pageable pageable);
+
+    ProductResponse detail(Long prodId);
+
+    Long create(ProductCreateRequest request);
+
+    void update(Long prodId, ProductUpdateRequest request);
+
+    void changeStatus(Long prodId, String status);
+
+    void delete(Long prodId);
 }
