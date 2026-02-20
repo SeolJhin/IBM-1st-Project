1b14e6eb07c0a2c06f805c08ceb4046c056ae0a4
hwangza125 <cutegirlji4282@gmail.com>
2026-02-20 18:25:30 +0900

    commoncode

--- uni-place-backend/src/main/java/org/myweb/uniplace/domain/commoncode/api/dto/response/GroupCommonCodeResponse.java
+++ uni-place-backend/src/main/java/org/myweb/uniplace/domain/commoncode/api/dto/response/GroupCommonCodeResponse.java
@@ -1,5 +1,29 @@
 package org.myweb.uniplace.domain.commoncode.api.dto.response;
 
+import lombok.*;
+import org.myweb.uniplace.domain.commoncode.domain.entity.GroupCommonCode;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class GroupCommonCodeResponse {
 
+    private String groupCode;
+    private String groupCodeName;
+    private String description;
+    private Integer isActive;
+    private LocalDateTime createdAt;
+
+    public static GroupCommonCodeResponse fromEntity(GroupCommonCode e) {
+        return GroupCommonCodeResponse.builder()
+                .groupCode(e.getGroupCode())
+                .groupCodeName(e.getGroupCodeName())
+                .description(e.getDescription())
+                .isActive(e.getIsActive())
+                .createdAt(e.getCreatedAt())
+                .build();
+    }
 }
