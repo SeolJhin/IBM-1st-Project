001f31420fb9612ab1bf6dc1cecb7fccffe98d36
hwangza125 <cutegirlji4282@gmail.com>
2026-02-27 10:31:20 +0900

    hub,css

--- uni-place-frontend/src/features/admin/pages/system/AdminSystemHub.jsx
+++ uni-place-frontend/src/features/admin/pages/system/AdminSystemHub.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-import styles from './AdminUserHub.module.css';
+import styles from './AdminSystemHub.module.css';
 
-export default function AdminUserHub() {
+export default function AdminSystemHub() {
   return (
     <div className={styles.wrap}>
       <div className={styles.topRow}>
-        <h1 className={styles.title}>회원관리</h1>
+        <h1 className={styles.title}>배너 관리</h1>
 
         {/* (원하면) 검색 아이콘/검색창 자리 */}
         <div className={styles.right} />
@@ -14,22 +14,31 @@ export default function AdminUserHub() {
 
       <div className={styles.tabs}>
         <NavLink
-          to="/admin/users"
+          to="/admin/system/banners"
           end
           className={({ isActive }) =>
             `${styles.tab} ${isActive ? styles.tabActive : ''}`
           }
         >
-          회원
+          배너
         </NavLink>
 
         <NavLink
-          to="/admin/users/residents"
+          to="/admin/affiliates"
           className={({ isActive }) =>
             `${styles.tab} ${isActive ? styles.tabActive : ''}`
           }
         >
-          입주자
+          제휴
+        </NavLink>
+
+        <NavLink
+          to="/admin/users/company"
+          className={({ isActive }) =>
+            `${styles.tab} ${isActive ? styles.tabActive : ''}`
+          }
+        >
+          회사
         </NavLink>
       </div>
 
