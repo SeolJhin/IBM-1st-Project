b9d94fb14e29ecc17868d5db27aef44c90d9bd43
hwangza125 <cutegirlji4282@gmail.com>
2026-02-28 18:03:06 +0900

    banner

--- uni-place-backend/src/main/java/org/myweb/uniplace/domain/system/api/dto/request/BannerOrderRequest.java
+++ uni-place-backend/src/main/java/org/myweb/uniplace/domain/system/api/dto/request/BannerOrderRequest.java
@@ -1,8 +1,9 @@
 package org.myweb.uniplace.domain.system.api.dto.request;
 
-import lombok.Getter;
+import lombok.*;
 
 @Getter
+@Setter
 public class BannerOrderRequest {
     private Integer banId;
     private Integer banOrder;
