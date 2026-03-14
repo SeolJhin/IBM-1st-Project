30fa4ef169dea9529b8b35b032cbe06b753a96ce
SeolJhin <rjw0417@gmail.com>
2026-03-14 19:42:54 +0900

    proxy/v2

--- uni-place-frontend/src/setupProxy.js
+++ uni-place-frontend/src/setupProxy.js
@@ -3,11 +3,11 @@ const proxyTarget = process.env.FRONTEND_PROXY_TARGET || "http://localhost:8080"
 
 module.exports = function (app) {
     app.use(
+        "/api",
         createProxyMiddleware({
             target: proxyTarget,
             changeOrigin: true,
             ws: true,
-            pathFilter: ["/api"],
         })
     );
 };
